66d547ffa6a5b59256b55efafefd1b5293817e49
ambrishh-choubey <ambrishchoubey9@gmail.com>
2026-05-12 10:54:54 +0530

    little change in  news api

--- news_API/main.py
+++ news_API/main.py
@@ -3,7 +3,8 @@ query=input("what type of news you wanna read?\n")
 api="3bcf1be722854a8aae762948282a511b"
 
 
-url=f"https://newsapi.org/v2/everything?q={query}&from=2026-01-24&sortBy=publishedAt&apiKey={api}"
+# url=f"https://newsapi.org/v2/everything?q={query}&from=2026-01-24&sortBy=publishedAt&apiKey={api}"
+url=f"https://newsapi.org/v2/everything?q={query}&from=2026-04-12&sortBy=publishedAt&apiKey={api}"
 print(url)
 r=requests.get(url)
 data =r.json()
